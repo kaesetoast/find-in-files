b6169c5ac9ef79a2fb65996a80f0e657cbfdda15
Philipp Nowinski <philipp@nowinski.de>
2014-12-02 10:51:26 +0100

    more tests

--- test/test.js
+++ test/test.js
@@ -3,27 +3,46 @@
 var chai = require('chai'),
     findInFiles = require('../index'),
     stringOne = 'dolor sit amet',
-    stringTwo = 'Träumen';
+    stringTwo = 'Träumen',
+    stringThree = 'This is in both files',
+    stringFour = 'This is duplicate';
 
 chai.should();
 
 describe('find some test strings', function () {
-    it('should find stringOne in fileOne exactly one time', function (done) {
+    it('should find stringOne only in fileOne exactly one time', function (done) {
         findInFiles.find(stringOne, '.', '.txt$')
         .then(function(result) {
             result['test/fileOne.txt'].count.should.equal(1);
+            result.should.not.have.property('test/fileTwo.txt');
             done();
         });
     });
 
-    it('should find stringTwo in fileTwo exactly one time', function (done) {
+    it('should find stringTwo only in fileTwo exactly one time', function (done) {
         findInFiles.find(stringTwo, '.', '.txt$')
         .then(function(result) {
             result['test/fileTwo.txt'].count.should.equal(1);
+            result.should.not.have.property('test/fileOne.txt');
             done();
-        })
-        .catch(function(result) {
-            console.log(result);
+        });
+    });
+
+    it('should find stringThree in both files exactly one time', function (done) {
+        findInFiles.find(stringThree, '.', '.txt$')
+        .then(function(result) {
+            result['test/fileOne.txt'].count.should.equal(1);
+            result['test/fileTwo.txt'].count.should.equal(1);
+            done();
+        });
+    });
+
+    it('should find stringFour 2 times in fileOne and 3 times in fileTwo', function (done) {
+        findInFiles.find(stringFour, '.', '.txt$')
+        .then(function(result) {
+            result['test/fileOne.txt'].count.should.equal(2);
+            result['test/fileTwo.txt'].count.should.equal(3);
+            done();
         });
     });
 });
